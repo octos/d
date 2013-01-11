60e8fab4752e2d5221ed198436ca65ca6f1c13c9
octos <octosquidopus@gmail.com>
2013-01-11 01:52:19 -0500

    simpler

--- dots.sh
+++ dots.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # .make.sh
-# Creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+# creates symlinks from ~ to dotfiles in ~/d
 
-dir=~/dots                    # dotfiles directory
-olddir=~/dots_old             # old dotfiles backup directory
-files=".bashrc .Xdefaults .xinitrc"    # list of files/folders to symlink in homedir
+dir=~/d                    # dotfiles dir
+olddir=~/d_old             # old dotfiles backup dir
+files=".bashrc .bash_profile .Xdefaults .xinitrc .mplayer"    # list of files/folders to symlink in homedir
 
 # create dots_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
@@ -19,7 +19,7 @@ echo "...done"
 # move any existing dots in homedir to dots_old directory, then create symlinks from the homedir to any files in the ~/dots directory specified in $files
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/$file ~/dots_old/
+    mv ~/$file ~/d_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/$file
 done
