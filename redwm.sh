b4086f117d957a96e8500cf0b098f730d6751689
octos <octosquidopus@gmail.com>
2013-02-27 17:12:15 -0500

    fixed redwm path

--- redwm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ver="130126" #recompiles dwm
-cd /home/kv/z/dwm
-urxvt -e "notify-send "recompiling dwm"
-makepkg -g >> PKGBUILD
-makepkg -efi --noconfirm
-cd ~
-notify-send "..done! restart it""
