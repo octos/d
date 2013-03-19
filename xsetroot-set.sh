59f9701eb346af91bf051c707bf16018ea38a393
octos <octosquidopus@gmail.com>
2013-03-19 08:21:57 -0400

    pomodoro, cleaner mute

--- xsetroot-set.sh
+++ xsetroot-set.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
-# updates xsetroot, is executed by ~/.xinitrc and dwm
-# once used "stlarch" font by https://bbs.archlinux.org/viewtopic.php?pid=1096029#p1096029
-#red=`ps cax | grep redshift | grep -o '^[ ]*[0-9]*'`
+# updates xsetroot, is executed by .xinitrc and dwm
 mute=`amixer get Master | tail -1 | cut -d " " -f 8`
-sound=`amixer get Master | tail -1 | sed 's/.*\[\([0-9]*%\)\].*/\1/'`
+vol=`amixer get Master | tail -1 | sed 's/.*\[\([0-9]*%\)\].*/\1/' | sed 's/%//'`
 cups=$(systemctl status cups cups-browsed | grep -c inactive)
-#kbd=$(setxkbmap -print | grep symbols | cut -d ' ' -f 6 | sed --n 's/([^()]*)//g' | sed 's/[^"]*+\([^"]*\)+[^"]*/\1 /g' | sed 's/ *\t.*//')
-kbdsimple=$(setxkbmap -print | grep symbols) 
+kbd=$(setxkbmap -print | grep symbols) 
+pomi=$(cat ~/.pomodoro) 
 
 case $mute in
-    "[off]") MUTE="($sound)" ;;
-    *) MUTE="$sound" ;;
+    "[off]") sound="$vol""M" ;;
+    *) sound="$vol%" ;;
 esac
 case $cups in 
     2) cups="";;
     *) cups="cups ";;
 esac
 
-case $kbdsimple in
+case $kbd in
     *+am*) kbd="Armn" ;;
    *+ara*) kbd="Arab" ;;
     *+ca*) kbd="Cans" ;;
@@ -28,7 +26,7 @@ case $kbdsimple in
     *+il*) kbd="Hebr" ;;
 *colemak*) kbd="Clmk" ;;
     *+us*) kbd=""     ;;
-        *) kbd="????" ;;
+        *) kbd="kbd?" ;;
 esac
 
-xsetroot -name "$(echo "$cups""$kbd" "$MUTE"; date +"%y%m%d %R")"
+xsetroot -name "$(echo "$pomi""$cups""$kbd" "$sound"; date +"%y%m%d %R")"
