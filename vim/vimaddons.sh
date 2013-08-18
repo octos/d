b153fc45658f69a7bdb195df1dd681eb50638261
octos <octosquidopus@gmail.com>
2013-08-18 15:45:56 -0400

    initial commit

---
+++ vim/vimaddons.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# installs vim addons I like
+# http://www.bestofvim.com/
+
+cd ~/.vim/bundle
+git clone https://github.com/plasticboy/vim-markdown.git #vim-markdown
+git clone https://github.com/mikewest/vimroom.git        #writing prose
+git clone git://github.com/tpope/vim-surround.git
+git clone https://github.com/bigfish/vim-js-beautify.git #use js-beautify from Vim
+
+echo "vim addons installed!"
+exit
