b3659eb4fff08c13f9c455a99e6227db1e8bf128
octos <octosquidopus@gmail.com>
2013-08-26 21:01:44 -0400

    customizable signal

--- killifrunning.sh
+++ killifrunning.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 a=$1
+sig=$2
 PIDS=`ps cax | grep $a | grep -o '^[ ]*[0-9]*'`
+if [[ $2 == "" ]]; then
+    sig=15; fi
 if [ -z "$PIDS" ]; then
   echo "$a was not running." 1>&2
   exit 1
 else
   for PID in $PIDS; do
-      kill -s 3 $PID #SIGTERM (15) is the default and safest kill. SIGQUIT (3) is safer.
-  done
-fi
+      kill -s $sig $PID #SIGTERM (15) is the default and safest kill. SIGQUIT (3) is safer.
+  done; fi
