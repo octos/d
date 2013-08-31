49a5dbafbd052ee5d2f374d9834dfae24441607a
octos <octosquidopus@gmail.com>
2013-08-30 23:06:35 -0400

    fixed:  was always interpreted as signal

--- toggle.sh
+++ toggle.sh
@@ -2,8 +2,9 @@
 a=$1
 sig=$2
 PIDS=`ps cax | grep $a | grep -o '^[ ]*[0-9]*'`
-if [[ $2 == "" ]]; then
-    sig=15; fi
+if ! [[ $2 =~ '^[0-9]+$' ]]; then #if $2 is not a number
+    sig=15
+fi
 if [ -z "$PIDS" ]; then
  echo "running $a"
   case $a in
