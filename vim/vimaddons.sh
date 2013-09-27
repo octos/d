b2fc782a42b99eb5a22866dd15927ac9df46fd7c
octos <octosquidopus@gmail.com>
2013-09-26 23:22:12 -0400

    support for colorschemes

--- vim/vimaddons.sh
+++ vim/vimaddons.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
-# installs vim addons I like
+# installs vim plugins
 # http://www.bestofvim.com/
 
+# plugins
+mkdir -p ~/.vim/bundle
 cd ~/.vim/bundle
 git clone https://github.com/plasticboy/vim-markdown.git #vim-markdown
 git clone https://github.com/mikewest/vimroom.git        #writing prose
 git clone git://github.com/tpope/vim-surround.git
 git clone https://github.com/bigfish/vim-js-beautify.git #use js-beautify from Vim
 git clone https://github.com/godlygeek/csapprox.git      #make gvim-only coloschemes work in terminal
+echo "installed vim plugins."
 
-echo "vim addons installed!"
+# themes
+mkdir -p ~/.vim/colors
+cd ~/.vim/colors
+git clone git://github.com/chriskempson/base16-vim.git base16; cp base16/colors/*.vim . #base16
+echo "installed vim colorschemes."
+
 exit
