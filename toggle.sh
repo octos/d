f85fc24aecb3a0b94a400ab5f3b9f7cb3a7bf5c3
octos <octosquidopus@gmail.com>
2013-09-26 23:07:28 -0400

    version, KISS

--- toggle.sh
+++ toggle.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-a=$1
+ver=190926 # toggles $1 with custom arguments
 sig=$2
-PIDS=`ps cax | grep $a | grep -o '^[ ]*[0-9]*'`
+PIDS=`ps cax | grep $1 | grep -o '^[ ]*[0-9]*'`
 if ! [[ $2 =~ '^[0-9]+$' ]]; then #if $2 is not a number
     sig=15
 fi
 if [ -z "$PIDS" ]; then
- echo "running $a"
-  case $a in
+ echo "running $1"
+  case $1 in
     pomi.sh) ~/d/pomi.sh;;
-    redshift) redshift -l 45.427778:-75.710556 &;;
+    redshift) redshift -l 45.425:-75.70 &;;
     unclutter) unclutter -noevents -idle 1 &;;
-    *) $a;;
+    *) $1;;
   esac
  exit
 else
  for PID in $PIDS; do
-    echo "killing $a"
-  case $a in #pomi.sh needs to be killed with sig 9
-   pomi.sh) kill -s 9 $PID; rm ~/.pomodoro; notify-send -u low -t 100 "pomi stopped"; ~/d/xsetroot-set.sh;;
-         *) kill -s $sig $PID;;
-  esac
+   echo "killing $1"
+   case $1 in #pomi.sh needs to be killed with sig 9
+     pomi.sh) kill -s 9 $PID; rm ~/.pomodoro; notify-send -u low -t 100 "pomi stopped"; ~/d/xsetroot-set.sh;;
+     *) kill -s $sig $PID;;
+   esac
  done
 fi
