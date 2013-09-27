06e4a036f394a66e19360e0851989115dcb2609d
octos <octosquidopus@gmail.com>
2013-09-26 20:00:22 -0400

    KISS

--- killifrunning.sh
+++ killifrunning.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-a=$1
 sig=$2
-PIDS=`ps cax | grep $a | grep -o '^[ ]*[0-9]*'`
+PIDS=`ps cax | grep $1 | grep -o '^[ ]*[0-9]*'`
 if [[ $2 == "" ]]; then
     sig=15; fi
 if [ -z "$PIDS" ]; then
-  echo "$a was not running." 1>&2
+  echo "$1 was not running." 1>&2
   exit 1
 else
   for PID in $PIDS; do
