1c6bf02be3819eec0a3b93c959d419158d1df633
octos <octosquidopus@gmail.com>
2013-09-27 20:37:14 -0400

    minor

--- print.sh
+++ print.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-ver=130927 #toggles CUPS. To work, user must be allowed to passwordlessly execute systemctl (in visudo).
-#TODO: if dwb not running, open in simpler browser.
+ver=130927 # toggles CUPS. User must be allowed to passwordlessly execute systemctl (in visudo).
+#TODO: if dwb not running, use simpler browser.
 r='\e[0;31m' g='\e[0;32m' x='\e[0m'
 
 cups=$(systemctl status cups cups-browsed | grep -c inactive)
 
 if [[ $cups == 2 ]]; then
-  echo -e "${g}starting${x} cups cups-browsed.." 
+  echo -e "${g}starting${x} cups cups-browsed.."
   sudo systemctl start cups cups-browsed &&
   sleep 2; dwb localhost:631; exit
 else
