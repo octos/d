bbb7aef5bf619a1fee838bfe5898dbfc348ca855
octos <octosquidopus@gmail.com>
2015-09-25 15:39:07 -0400

    change git: to https:

--- vim/vimaddons.sh
+++ vim/vimaddons.sh
@@ -7,7 +7,7 @@ mkdir -p ~/.vim/bundle
 cd ~/.vim/bundle
 git clone https://github.com/plasticboy/vim-markdown.git #vim-markdown
 git clone https://github.com/mikewest/vimroom.git        #writing prose
-git clone git://github.com/tpope/vim-surround.git
+git clone https://github.com/tpope/vim-surround.git
 git clone https://github.com/bigfish/vim-js-beautify.git #use js-beautify from Vim
 git clone https://github.com/godlygeek/csapprox.git      #make gvim-only coloschemes work in terminal
 echo "installed vim plugins."
@@ -15,7 +15,8 @@ echo "installed vim plugins."
 # themes
 mkdir -p ~/.vim/colors
 cd ~/.vim/colors
-git clone git://github.com/chriskempson/base16-vim.git base16; cp base16/colors/*.vim . #base16
+cd ~/.vim/bundle
+git clone https://github.com/chriskempson/base16-vim.git base16; cp base16/colors/*.vim . #base16
 echo "installed vim colorschemes."
 
 exit
