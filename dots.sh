8071ca70e20690a8912c933c565d9abf93ad24e1
octos <octosquidopus@gmail.com>
2015-09-28 09:59:01 -0400

    rm mplayer, rm -e options, -i looks in /apps

--- dots.sh
+++ dots.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-ver=130926 # creates symlinks from ~ to dotfiles in ~/d
+ver=150928 # creates symlinks from ~ to dotfiles in ~/d
 
-dir=~/d                    # dotfiles dir
-olddir=~/d_old             # old dotfiles backup dir
+dir=~/c/d                    # dotfiles dir.
+olddir=~/c/d_old             # old dotfiles backup dir
 files="bashrc bash_profile iocanerc mailcap muttrc muttrc_aliases sxiv tmux.conf Xdefaults xinitrc vimrc
-config mplayer mpv themes vim"    # list of files/folders to symlink in homedir
+config mpv themes vim"    # list of files/folders to symlink in homedir
 
-if [[ $1 == "-i" ]]; then cd $dir
-    for f in `ls *.sh`; do echo -e "$f # `sed -n '2p' $f`"; done | column -t -s '#'; exit
+if [[ $1 == "-i" ]]; then cd $dir/app
+    for f in `ls *.sh`; do echo "$f # `sed -n '2p' $f`"; done | column -t -s '#'; exit
 fi
 
 killall -q chromium #because it makes .config/ busy
@@ -18,8 +18,8 @@ echo "   cd: $dir"; cd $dir
 # then create symlinks from ~ to all $files in $dir
     echo "   mv: old dotfiles from ~ to $olddir"
 for file in $files; do
-    mv ~/.$file ~/d_old/
-    echo "ln -s: symlinking ~/d/$file"
+    mv ~/.$file $olddir
+    echo "ln -s: symlinking $dir/$file"
     ln -s $dir/$file ~/.$file
 done
-    echo -e "done.\ndotfile info: $0 -i\n"
+    echo "Done. dotfile info: $0 -i"; echo
