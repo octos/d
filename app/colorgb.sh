79468e62b7af2b70231094406296f877401d063e
octos <octosquidopus@gmail.com>
2015-09-28 23:10:35 -0400

    better description

--- app/colorgb.sh
+++ app/colorgb.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# color picker by kv 130109. execute from dwm.
+ver=130109 # color picker by kv. Execute from dwm.
 #picked=`xoris -N | sed 's/#//'`
 picked=`printf "%02X%02X%02X\n" $(stint)`
 luma=$(echo $((0x$picked)) )
