2469dd246e6e9c464f3c66259d5ea9c62255e1e9
octos <octosquidopus@gmail.com>
2015-09-29 16:46:38 -0400

    fix sdf url

--- app/radio.sh
+++ app/radio.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-ver=150928X # online radio stations made easy
+#v.150929X online radio player
 case $1 in
   2f30|prog) station="http://radio.2f30.org:8000/live.ogg" ;;
-  sdf) station="http://anonradio.net/listen" ;;
+  sdf) station="http://anonradio.net:8000/anonradio" ;;
   94|country) station="http://ckklfm-mp3.akacast.akamaistream.net/7/541/102120/v1/astral.akacast.akamaistream.net/ckklfm-mp3" ;;
 esac
 
