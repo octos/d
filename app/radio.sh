0fe45672287c993866278a8ae3fb191a2633b5d1
octos <octosquidopus@gmail.com>
2015-09-30 12:08:50 -0400

    can use q to quit, no input=help, +kexp

--- app/radio.sh
+++ app/radio.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-#v.150929X online radio player
+#v.150930X online radio player
 case $1 in
-  2f30|prog) station="http://radio.2f30.org:8000/live.ogg" ;;
-  sdf) station="http://anonradio.net:8000/anonradio" ;;
-  94|country) station="http://ckklfm-mp3.akacast.akamaistream.net/7/541/102120/v1/astral.akacast.akamaistream.net/ckklfm-mp3" ;;
-esac
-
-mpv -no-cache $station 2>&1 >/dev/null
+  prog|2f30) s="http://radio.2f30.org:8000/live.ogg" ;;
+  sdf) s="http://anonradio.net:8000/anonradio" ;;
+  country|94) s="http://ckklfm-mp3.akacast.akamaistream.net/7/541/102120/v1/astral.akacast.akamaistream.net/ckklfm-mp3" ;;
+  kexp|903) s="http://live-aacplus-64.kexp.org/kexp64.aac" ;;
+  *) less $0 | sed '1d' ;; esac
+mpv -no-cache --really-quiet $s
