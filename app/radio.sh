48d330b65e74651064efe45a885169e257d4b502
octos <octosquidopus@gmail.com>
2015-09-30 15:18:17 -0400

    edit itself (-e), skip KEXP intro

--- app/radio.sh
+++ app/radio.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 #v.150930X online radio player
-case $1 in
-  prog|2f30) s="http://radio.2f30.org:8000/live.ogg" ;;
-  sdf) s="http://anonradio.net:8000/anonradio" ;;
-  country|94) s="http://ckklfm-mp3.akacast.akamaistream.net/7/541/102120/v1/astral.akacast.akamaistream.net/ckklfm-mp3" ;;
-  kexp|903) s="http://live-aacplus-64.kexp.org/kexp64.aac" ;;
-  eo) s="http://listen.radionomy.com/muzaikoinfo.m3u" ;;
+s=0; case $1 in
+  prog|2f30) o="http://radio.2f30.org:8000/live.ogg" ;;
+  sdf) o="http://anonradio.net:8000/anonradio" ;;
+  country|94) o="http://ckklfm-mp3.akacast.akamaistream.net/7/541/102120/v1/astral.akacast.akamaistream.net/ckklfm-mp3" ;;
+  kexp|903) s=15 o="http://live-aacplus-64.kexp.org/kexp64.aac" ;;
+  eo) o="http://listen.radionomy.com/muzaikoinfo.m3u" ;;
+  -e) $EDITOR $0 ;;
   *) less $0 | sed '1d' ;; esac
-mpv -no-cache -really-quiet $s
+mpv -no-cache -really-quiet --force-seekable=yes -start $s $o
