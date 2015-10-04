5c5c877b897ec90ac383c220ccba4b35ab0b179c
octos <octosquidopus@gmail.com>
2015-10-04 17:30:15 -0400

    minor improvement

--- app/toggle.sh
+++ app/toggle.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
-#v.151004X toggles $1 with custom sig
+#v.151004X toggles $1 (optional custom SIG)
 sig=$2
 case $1 in
   *noise.sh|noise) realname=play;;
   *pomi.sh) sig=6;; #pomi.sh must be killed with sig 9
   -e) $EDITOR $0; exit;;
   -l) ps cax; exit;;
-  ""|-*) echo "Usage: $0 [-e] program_to_toggle"; exit;;
+  ""|-*) echo "Usage: $0 [-e] \"program [ARGUMENT]\" SIG"; exit;;
   *) realname=$1;;
 esac
 
 PIDS=`ps cax | grep $realname| grep -o '^[ ]*[0-9]*'`
-if ! [[ $2 =~ '^[0-9]+$' ]]; then #if $2 is not a number
-    sig=15
-fi
+case $2 in  ''|*[!0-9]*) sig=15;; esac #if $2 is not a number
+
 if [ -z "$PIDS" ]; then
- echo "running $@"
+ echo "running $1"
   case $1 in
     pomi.sh) ~/c/d/app/pomi.sh;;
     *noise.sh|noise) ~/c/d/app/noise.sh &;;
     redshift) redshift -l 45.425:-75.70 &;;
     unclutter) unclutter -noevents -idle 1 &;;
-    *) $@ &;;
+    *) $1 &;;
   esac
  exit
 else
